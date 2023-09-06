62063ac1892c4d5a060399f147495e8a49a7a4a5
Malte Ehrlen <malte.ehrlen@freiheit.com>
2023-09-06 12:40:33 +0300

    some changes

--- terror.go
+++ terror.go
@@ -4,30 +4,45 @@ type TryFunc[R any] func() R
 type CatchFunc[R any] func(Catchable) R
 type FinallyFunc[R any] func(R) R
 type Catchable any
+type TryBlock[R any] struct {
+	Try     TryFunc[R]
+	Catch   CatchFunc[R]
+	Finally FinallyFunc[R]
+}
+
+func (b TryBlock[R]) run() R {
+	if b.Catch != nil {
+		if b.Finally != nil {
+			return terror(b.Try, b.Catch, b.Finally)
+		}
+		tryCatch(b.Try, b.Catch)
+	} else if b.Finally != nil {
+		tryFinally(b.Try, b.Finally)
+	}
+	return b.Try()
+}
+
 func Throw(v Catchable) {
-    panic(v)
+	panic(v)
 }
-func TryCatch[R any](try TryFunc[R], catch CatchFunc[R]) R {
-    return terror[R](try, catch, func(r R) (R) {return r})
+func tryCatch[R any](try TryFunc[R], catch CatchFunc[R]) R {
+	return terror[R](try, catch, func(r R) R { return r })
 }
-func TryFinally[R any](try TryFunc[R], finally FinallyFunc[R]) R {
-    catchFunc := func(c Catchable) (R) {
-        Throw(c)
-        return *new(R)
-    }
-    return terror[R](try, catchFunc, finally)
+func tryFinally[R any](try TryFunc[R], finally FinallyFunc[R]) R {
+	catchFunc := func(c Catchable) R {
+		Throw(c)
+		return *new(R)
+	}
+	return terror[R](try, catchFunc, finally)
 }
-func TryCatchFinally[R any](try TryFunc[R], catch CatchFunc[R], finally FinallyFunc[R]) R {
-    return terror[R](try, catch, finally)
-}
-func terror[R any] (try TryFunc[R], catch CatchFunc[R], finally FinallyFunc[R]) (r R) {
-    defer func() {
-        if c := recover(); c != nil {
-            r = catch(c)
-            r = finally(r)
-        }
-    }()
-    r = try()
-    r = finally(r)
-    return
+func terror[R any](try TryFunc[R], catch CatchFunc[R], finally FinallyFunc[R]) (r R) {
+	defer func() {
+		if c := recover(); c != nil {
+			r = catch(c)
+			r = finally(r)
+		}
+	}()
+	r = try()
+	r = finally(r)
+	return
 }
